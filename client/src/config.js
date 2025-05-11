7fe0c1741b9b334745b0436dd5032840460a6c07
emcommit <emilicel@gmail.com>
2025-05-11 19:06:07 +0100

    Update API_URL to use public Railway URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 // Use a relative path for Netlify Functions
-export const API_URL = 'https://restaurantprofitpro.onrender.com/api/menus';
+export const API_URL = 'https://restaurantprofitprobackend-production.up.railway.app/api/menus';
